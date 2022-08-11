cbee31a1e18429b3cd612044b8823f2cb324d6ba
Bokkoa <krivozznova@gmail.com>
2022-08-10 23:01:50 -0500

    Adding more configuration and a command line runner

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -15,3 +15,15 @@ INSERT INTO clients (region_id, name, last_name, email, created_at) VALUES(2, 'M
 INSERT INTO clients (region_id, name, last_name, email, created_at) VALUES(6, 'Kira', 'Ansuz', 'kira@example.com', '2022-11-30');
 INSERT INTO clients (region_id, name, last_name, email, created_at) VALUES(7, 'Mane', 'Ansuz', 'mane@example.com', '2022-11-30');
 INSERT INTO clients (region_id, name, last_name, email, created_at) VALUES(1, 'Ginger', 'Ansuz', 'ginger@example.com', '2022-11-30');
+
+
+
+INSERT INTO users (username, password, enabled) VALUES ("konejo", '', 1);
+INSERT INTO users (username, password, enabled) VALUES ("blaz", '', 1);
+
+INSERT INTO roles (name) VALUES ('ROLE_USER')
+INSERT INTO roles (name) VALUES ('ROLE_ADMIN')
+
+INSERT INTO user_roles (user_id, role_id) VALUES (1, 1);
+INSERT INTO user_roles (user_id, role_id) VALUES (2, 2);
+INSERT INTO user_roles (user_id, role_id) VALUES (2, 1);
